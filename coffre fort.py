be7351d676220be2e708be09408389743aa4b90d
strouilh <sylvie.trouilhet@gmail.com>
2023-02-07 22:30:57 +0100

    Add files via upload

---
+++ coffre fort.py
@@ -0,0 +1,16 @@
+# coffre fort
+def coffre_fort(m,d,c,u):
+    rep = m%2==0 and m+c==15 and m+c==15 and d==c-m and m==u*d
+    return rep
+
+def decomposer(nb):
+    u=nb%10
+    d=int(nb/10)%10
+    c=int(nb/100)%10
+    m=int(nb/1000)%10
+    return(m,c,d,u)
+
+for nb in range(1000,9999):
+    (m,c,d,u)=decomposer(nb)
+    if coffre_fort(m,c,d,u):
+        print(nb)
